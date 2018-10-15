b31a9b6ef435c34f715d492b194651494d17789f
LizLeadley <elizabeth.leadley@students.olin.edu>
2018-10-15 11:50:57 -0400

    basic webcam feed

---
+++ art.py
@@ -0,0 +1,21 @@
+""" Experiment with face detection and image filtering using OpenCV """
+
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # comvert frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Create window displaying grayscale
+    cv2.imshow('frame', gray)
+    # Wait between frames. Changing this is how slow and fast motion happen
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+# Unbind video capture object and close any created windows.
+cap.release()
+cv2.destroyAllWindows()
