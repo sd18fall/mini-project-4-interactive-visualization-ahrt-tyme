19168c7c2acae634f4466ed004d85567e6aa59fd
LizLeadley <elizabeth.leadley@students.olin.edu>
2018-10-16 19:37:08 -0400

    find changes in pixel average color value

--- art.py
+++ art.py
@@ -2,19 +2,40 @@
 
 import cv2
 import numpy as np
+import math
 
 cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture('~/Toolboxes/CompVis/Spin.avi')
+
+def findMove(frame, lastFrame):
+    """Detects changing pixel colors and reports their x,y coordinates"""
+    changes = [] #list of x,y tuples of pixels that changed
+    for y in range(height):
+        for x in range(width):
+            avgVal = np.mean(frame[y][x][:])
+            lastAvgVal = np.mean(lastFrame[y][x][:])
+            if abs(avgVal-lastAvgVal) > 25:
+                changes.append((x,y))
+    return changes
+
+
+ret, lastFrame = cap.read()
+height, width = lastFrame.shape[:2]
 
 while(True):
     ret, frame = cap.read()
     # comvert frame to grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    changes = findMove(frame, lastFrame)
+    #changes is a list of tuples of x,y values
 
     # Create window displaying grayscale
-    cv2.imshow('frame', gray)
+    cv2.imshow('frame', frame)
     # Wait between frames. Changing this is how slow and fast motion happen
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(0) & 0xFF == ord('q'):
         break
+
+    lastFrame = frame
 
 
 # Unbind video capture object and close any created windows.
